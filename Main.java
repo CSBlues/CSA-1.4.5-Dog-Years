082bae11776670477448447e4bdb152a03e795d8
CSBlues <66141547+CSBlues@users.noreply.github.com>
2020-06-17 19:53:00 -0500

    removed nextlines

--- Main.java
+++ Main.java
@@ -6,19 +6,15 @@ class Main {
     int currentYear = 0;
     int dogBirthYear = 0;
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter your birth year: ");
+    System.out.println("Enter your birth year: ");
     // TODO: Set birth year equal to the input value.
 
-    input.nextLine();
-    System.out.print("Enter your dog's birth year: ");
+    System.out.println("Enter your dog's birth year: ");
     // TODO: Set dogBirthYear equal to the input.
     
-    input.nextLine();
-
-    System.out.print("Enter the current year: ");
+    System.out.println("Enter the current year: ");
     // TODO: Set the currentYear equal to the input.
 
-    input.nextLine();
 
     /* TODO: 
     Implement the algorithms to calculate your 
